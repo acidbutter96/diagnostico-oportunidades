bfd18ace8449a7f3b5e00caf4c8414c7f5be982c
acidbutter96 <marknotas@outlook.com>
2020-04-23 02:08:35 -0300

    Visualizar dados para verificar relações

--- diagnostico_oportunidade.py
+++ diagnostico_oportunidade.py
@@ -6,6 +6,9 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#configurações do seaborn
+
+sns.set_style('darkgrid')
 
 
 #Analisar o head do Data Frame
@@ -14,7 +17,7 @@ pd.read_csv('Data_Base.csv', encoding = 'ISO-8859-1').head()
 
 #Definir nome das colunas
 
-colunas = ['Codigo', 'Data/Hora Dia',  'Mês', 'Ano', 'D#', 'Código', 'Código 2', 'Código 3', 'Tipo de venda', 'UF', 'Código 4', 'Família', 'Produto 1', 'ABC',  'Produto 2', 'Venda Bruta col', 'Venda Bruta 1', 'Venda Bruta 2', 'Venda Bruta 3']
+colunas = ['ID', 'Data/Hora Dia',  'Mês', 'Ano', 'D#', 'Código', 'Código 2', 'Código 3', 'Tipo de venda', 'UF', 'Código 4', 'Família', 'Produto 1', 'ABC',  'Produto 2', 'Venda Bruta col', 'Venda Bruta 1', 'Venda Bruta 2', 'Venda Bruta 3']
 
 df = pd.read_csv('Data_Base.csv',names=colunas, encoding = 'ISO-8859-1')
 
@@ -52,4 +55,12 @@ for i in index:
 colunas.remove('Mês')
 df.drop('Mês', inplace=True, axis=1)
 
-#
+#Verificar o restante dos dados a partir das correlações entre as colunas
+
+def heatmap():
+	crrdf = df.corr()
+	fig = plt.figure(figsize=(10,5),dpi=200)
+	plt.tight_layout()
+	sns.heatmap(crrdf).set_title('Mapa de Calor - Correlação Entre Dados').get_figure().savefig('heatmap.png')
+
+heatmap()
